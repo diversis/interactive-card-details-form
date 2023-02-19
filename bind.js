b780ef5a0f51a914841a2ec9eb9af8e3d0924ce0
Noxyangel <forgotrrr@gmail.com>
2023-02-20 01:15:52 +0300

    regex

--- bind.js
+++ bind.js
@@ -1,13 +1,20 @@
 export function setupCardNumberBind(input, element) {
-    console.log("input: ", input, "\nelement: ", element);
-    let number = "";
-
     input.oninput = () => {
-        if (!input.value) {
+        let inputValue = input.value.trim();
+        if (!inputValue) {
             element.innerHTML = "0000 0000 0000 0000";
+            input.setAttribute("value", "");
         }
-        if (/\d{16}/.test(input.value)) {
-            const newCard = input.value.match(/\d{16}/)[0];
+
+        if (inputValue.length > 16) {
+            inputValue = inputValue.substring(0, 16);
+            input.value = inputValue;
+        }
+        const match = inputValue.match(/^\d{1,16}$/s);
+        if (!!match && match.length > 0) {
+            const newCard = match[0];
+            input.setAttribute("value", newCard);
+
             element.innerHTML =
                 "" +
                 newCard.substring(0, 4) +
@@ -23,24 +30,89 @@ export function setupCardNumberBind(input, element) {
 1234567890987654;
 
 export function setupCardholderNameBind(input, element) {
-    console.log("input: ", input, "\nelement: ", element);
-    let number = "";
-
     input.oninput = () => {
-        if (!input.value) {
+        let inputValue = input.value.trim();
+        if (!inputValue) {
             element.innerHTML = "JANE APPLESEED";
+            input.setAttribute("value", "");
         }
-        if (/\d{16}/.test(input.value)) {
-            const newCard = input.value.match(/\d{16}/)[0];
-            element.innerHTML =
-                "" +
-                newCard.substring(0, 4) +
-                " " +
-                newCard.substring(4, 8) +
-                " " +
-                newCard.substring(8, 12) +
-                " " +
-                newCard.substring(12);
+
+        if (inputValue.length > 26) {
+            inputValue = inputValue.substring(0, 26);
+            input.value = inputValue;
+        }
+        const match = inputValue.match(/^(?<! )[-a-zA-Z' ]{2,26}$/s);
+        if (!!match && match.length > 0) {
+            const newCardholder = match[0];
+            input.setAttribute("value", newCardholder);
+            element.innerHTML = newCardholder;
+        }
+    };
+}
+
+export function setupCardCVCBind(input, element) {
+    input.oninput = () => {
+        let inputValue = input.value.trim();
+        if (!inputValue) {
+            element.innerHTML = "000";
+            input.setAttribute("value", "");
+        }
+
+        if (inputValue.length > 3) {
+            inputValue = inputValue.substring(0, 3);
+            input.value = inputValue;
+        }
+        const match = inputValue.match(/^\d{1,3}$/s);
+        if (!!match && match.length > 0) {
+            const newCVC = inputValue.match(/^\d{1,3}$/s)[0];
+            input.setAttribute("value", newCVC);
+
+            element.innerHTML = newCVC;
+        }
+    };
+}
+
+export function setupCardExpMMBind(input, element) {
+    input.oninput = () => {
+        let inputValue = input.value.trim();
+
+        console.log("input: ", inputValue, "\nelement: ", element);
+        if (!inputValue) {
+            element.innerHTML = "MM";
+            input.setAttribute("value", "");
+        }
+
+        if (inputValue.length > 2) {
+            inputValue = inputValue.substring(0, 2);
+            input.value = inputValue;
+        }
+        const match = inputValue.match(/^([0][1-9])|([1][0-2])$/s);
+        if (!!match && match.length > 0) {
+            const newExpMM = match[0];
+
+            input.setAttribute("value", newExpMM);
+            element.innerHTML = newExpMM;
+        }
+    };
+}
+
+export function setupCardExpYYBind(input, element) {
+    input.oninput = () => {
+        let inputValue = input.value.trim();
+        if (!inputValue) {
+            element.innerHTML = "YY";
+            input.setAttribute("value", "");
+        }
+
+        if (inputValue.length > 2) {
+            inputValue = inputValue.substring(0, 2);
+            input.value = inputValue;
+        }
+        const match = inputValue.match(/^\d{1,2}$/s);
+        if (!!match && match.length > 0) {
+            const newExpYY = match[0];
+            input.setAttribute("value", newExpYY);
+            element.innerHTML = newExpYY;
         }
     };
 }
--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-import { setupCardNumberBind } from "./bind.js";
+import {
+    setupCardNumberBind,
+    setupCardCVCBind,
+    setupCardholderNameBind,
+    setupCardExpMMBind,
+    setupCardExpYYBind,
+} from "./bind.js";
 
 setupCardNumberBind(
     document.querySelector("#card-number-input"),
     document.querySelector("#card-front-number")
+);
+
+setupCardCVCBind(
+    document.querySelector("#card-cvc-input"),
+    document.querySelector("#card-back-cvc")
+);
+
+setupCardholderNameBind(
+    document.querySelector("#cardholder-name"),
+    document.querySelector("#card-front-cardholder")
+);
+
+setupCardExpMMBind(
+    document.querySelector("#expiration-date-mm"),
+    document.querySelector("#card-front-mm")
+);
+
+setupCardExpYYBind(
+    document.querySelector("#expiration-date-yy"),
+    document.querySelector("#card-front-yy")
 );
